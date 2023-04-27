6c61bc134f03a398ec1d19fce6f383e8534bac6d
niklasschoening <115229573+niklasschoening@users.noreply.github.com>
2023-04-27 15:28:55 +0200

    Add files via upload

---
+++ rsa.py
@@ -0,0 +1,26 @@
+import random
+
+def getPrime(x,y):      
+    isPrime = False
+    if x<2: x=2
+
+    while not isPrime:
+        pPrime=random.randrange(x,y)                #random Zahl in range wird generiert
+        isPrime=True
+        for i in range(2,pPrime):
+            if pPrime % i == 0: isPrime = False
+        
+    return pPrime
+
+def getPrime2(x,y):
+    isPrime = False
+    if x<2: x=2
+
+    while not isPrime:
+        pPrime=random.randrange(x,y)                #random Zahl in range wird generiert
+        if pow(2,(pPrime-1))%pPrime==1:
+            isPrime=True
+        
+    return pPrime
+
+print(getPrime2(0,1000))
